8ef345e3685e7d92b3e14ece08d95a962752dad4
Lee Yoon Seok <kjggr123@gmail.com>
2024-08-06 19:18:01 +0900

    간단한 플레이어 입력 스크립트 생성 및 맵 충돌체크 확인

--- Assets/LeeProject/Character/PlayerInput.cs
+++ Assets/LeeProject/Character/PlayerInput.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float h;
+    private float v;
+    public float speed;
+
+    private Rigidbody2D rigid;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v) * speed;
     }
 }
