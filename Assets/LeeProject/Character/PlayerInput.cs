58664076f4994ad832efb9a6dfc117a8f715b8b0
Lee Yoon Seok <kjggr123@gmail.com>
2024-08-06 23:25:41 +0900

    캐릭터 상하좌우 이동 구현 및 Idle/Walk 애니메이션 연결

--- Assets/LeeProject/Character/PlayerInput.cs
+++ Assets/LeeProject/Character/PlayerInput.cs
@@ -6,9 +6,14 @@ public class PlayerInput : MonoBehaviour
 {
     private float h;
     private float v;
+
     public float speed;
+    //
+    private bool bIsKeyDowned;
+    private bool bMoveHorizontal;
 
     private Rigidbody2D rigid;
+    private Animator anim;
 
 
     void Start()
@@ -19,16 +24,52 @@ public class PlayerInput : MonoBehaviour
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        bool bHorizontalDown = Input.GetButtonDown("Horizontal");
+        bool bVerticalDown = Input.GetButtonDown("Vertical");
+        bool bHorizontalUp = Input.GetButtonUp("Horizontal");
+        bool bVerticalUp = Input.GetButtonUp("Vertical");
+
+        if (bHorizontalDown || bVerticalUp)
+        {
+            bMoveHorizontal = true;
+        }
+        else if(bVerticalDown || bHorizontalUp)
+        {
+            bMoveHorizontal = false;
+        }
+
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("bIsChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if(anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("bIsChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("bIsChange", false);
+        }
+        
+        
+        
     }
+
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v) * speed;
+        Vector2 moveDir = bMoveHorizontal ? new Vector2(h, 0) : new Vector2(0, v);
+
+        rigid.velocity = moveDir * speed;
     }
 }
